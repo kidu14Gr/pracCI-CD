2f9991bb9dbccb88c52cf33fb5279946a1e76d69
kidu14gr <kidugirma24@gmail.com>
2025-07-29 12:10:42 +0300

    chang appears

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-def say_hello():
-    print("don't do it !")
+import sys
+
+def say_hello(name: str) -> str:
+    return f"Hello, {name}! This is GitHub Actions at work."
 
 if __name__ == "__main__":
-    say_hello()
+    if len(sys.argv) > 1:
+        user_name = sys.argv[1]
+    else:
+        user_name = "Developer"
+    
+    greeting = say_hello(user_name)
+    print(greeting)
